1f5b3f82228a39eb9bbd95788fbe518bca493311
Alexander <148794699+alexanderrothschild@users.noreply.github.com>
2023-11-09 11:40:10 +0100

    restore default

--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -1,12 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import 'app.scss'
-
 
 createApp(App).mount('#app')
-App.use(BootstrapVue)
-App.use(IconsPlugin)
