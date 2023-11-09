ce66a4f8e7df3e6ec5943322ffed49d8fa5cb29a
Alexander <148794699+alexanderrothschild@users.noreply.github.com>
2023-11-09 16:21:11 +0100

    add bootstrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// Import our custom CSS
+import './scss/styles.scss'
+
+// Import only the Bootstrap components we need
+
 createApp(App).mount('#app')
