83937d46381e985e7b69018994bc26bd0ef8e872
Angelina <angelina.heinrichs@studium.uni-hamburg.de>
2023-11-21 16:09:35 +0100

    Updated scenario list
    Created a modern list with placeholders for scenarios. Added "Search" and "New" buttons as well as a silde bar for the list. "New"-button creates more placeholders in the list.
    
    Issue ID: #11
    
    Authored-by: Angelina Heinrichs

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
+<script setup>
+import { ref, computed } from 'vue'
+import Dashboard from '@/components/Dashboard.vue'
+import Scenarios from "@/components/Scenarios.vue";
+
+const routes = {
+  '/': Dashboard,
+  '/scenarios': Scenarios
+}
+
+const currentPath = ref(window.location.hash)
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+})
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/']
+})
+
+</script>
 
 <template>
-  <img alt="Dashboard logo" src="./assets/logo-dash.jpeg">
-  <DashBoard msg="Welcome to Your Dashboard"/>
+  <component :is="currentView" />
+  <a href="#/">Dashboard</a> |
+  <a href="#/scenarios">Scenarios</a>
 </template>
-
-<script>
-import Dashboard from "@/components/Scenarios.vue"; // change the .vue file that is displayed here
-
-export default {
-  name: 'App',
-  components: {
-    DashBoard: Dashboard
-  }
-}
-</script>
 
 <style>
 #app {
--- src/components/Scenarios.vue
+++ src/components/Scenarios.vue
@@ -1,29 +1,113 @@
-<script setup>
-import { ref } from 'vue';
+<script>
 
-const header = ref('Scenarios')
-const items = ref([
-  { id: 1, label: "#1" },
-  { id: 2, label: "#2" },
-  { id: 3, label: "#3" }
-  ])
+export default {
+  name: "ScenarioList",
+  data() {
+    return{
+      items:[
+        { id: 1, label: "#1" },
+        { id: 2, label: "#2" },
+        { id: 3, label: "#3" },
+        { id: 4, label: "#4" },
+        { id: 5, label: "#5" },
+        { id: 6, label: "#6" },
+        { id: 7, label: "#7" },
+        { id: 8, label: "#8" },
+      ],
+  };
+},
+  methods:{
+    newScenario() {
+      this.items.push({ id: this.items.length + 1, label: "#"+ (this.items.length + 1)});
+    }
+  }
+};
+
 </script>
 
 
 <template>
-  <h1>{{ header }}</h1>
-  <ul>
-      <li v-for="({id, label}) in items" :key="id">
-        {{ label }}
-      </li>
-    </ul>
+  <h1>Scenarios</h1>
+
+  <div class="btn-group">
+    <button class="search-button" @click="test">Search</button>
+    <button class="new-button" @click="newScenario">New</button>
+  </div>
+  <div class="list-container">
+    <div class="list-content">
+      <div v-for="item in items" :key="item.id" class="list-item">
+        {{ item.label }}
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
-  name: 'DashBoard',
-  props: {
-    msg: String
-  }
+
+<style scoped>
+.search-button {
+  background-color: rgb(146, 208, 223); /* Light Blue */
+  border: none;
+  color: white;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin-top: 10px;
+  cursor: pointer;
+  border-radius: 4px;
 }
-</script>
+
+.search-button:hover {
+  background-color: rgb(87, 144, 158);
+}
+.new-button {
+  background-color: rgb(114, 214, 101); /* Light Blue */
+  border: none;
+  color: white;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin-top: 10px;
+  cursor: pointer;
+  border-radius: 4px;
+}
+
+.new-button:hover {
+  background-color: rgb(73, 167, 61);
+}
+
+body {
+      font-family: 'Arial', sans-serif;
+      margin: 0;
+      padding: 0;
+      background-color: #f4f4f4;
+    }
+
+    .list-container {
+      max-width: 600px;
+      margin: 20px auto;
+      background-color: #fff;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+      border-radius: 8px;
+      overflow: hidden;
+      height: 400px; /* Set a fixed height for the container */
+    }
+
+    .list-item {
+      padding: 16px;
+      border-bottom: 1px solid #e0e0e0;
+      transition: background-color 0.3s;
+    }
+
+    .list-item:hover {
+      background-color: #f0f0f0;
+    }
+
+    .list-content {
+      max-height: 100%; /* Ensure that the content can take full height */
+      overflow-y: auto; /* Enable vertical scrollbar */
+    }
+</style>
