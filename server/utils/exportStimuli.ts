e242b9df8776bb7b3175ce1e2980add51bee2304
franksn90 <s.frankausowen@googlemail.com>
2024-11-01 21:30:00 +0100

    Add function to only use active stimuli for analysis

--- server/utils/exportStimuli.ts
+++ server/utils/exportStimuli.ts
@@ -1,10 +1,21 @@
 import fs from 'fs';
 
-export async function exportStimuli(formData: FormData, type: string, scenario: any) {
+export enum AnalysisType {
+    Simulation,
+    Monitoring
+}
+
+export async function exportStimuli(formData: FormData, type: string, scenario: any, analysisType: AnalysisType) {
     let fileContent = "";
-    for (let stimulus of scenario.stimuli!) {
-        fileContent = fileContent + stimulus.MTL + "\n"
+    switch (analysisType) {
+        case AnalysisType.Simulation:
+            fileContent = getSimulationMTLs(scenario)
+            break;
+        case AnalysisType.Monitoring:
+            fileContent = getMonitoringMTLs(scenario)
+            break;
     }
+
     let fileName = scenario.simulationID!
     await fs.promises.writeFile(fileName, fileContent);
 
@@ -12,4 +23,24 @@ export async function exportStimuli(formData: FormData, type: string, scenario: 
     const blob = new Blob([file], {type: 'application/octet-stream'});
     formData.append(type, blob, fileName)
     await fs.promises.unlink(fileName);
+}
+
+function getSimulationMTLs(scenario: any): string {
+    let fileContent = "";
+    for (let stimulus of scenario.stimuli!) {
+        if (stimulus.simulationChecked) {
+            fileContent = fileContent + stimulus.MTL + "\n"
+        }
+    }
+    return fileContent
+}
+
+function getMonitoringMTLs(scenario: any): string {
+    let fileContent = "";
+    for (let stimulus of scenario.stimuli!) {
+        if (stimulus.monitoringChecked) {
+            fileContent = fileContent + stimulus.MTL + "\n"
+        }
+    }
+    return fileContent
 }
